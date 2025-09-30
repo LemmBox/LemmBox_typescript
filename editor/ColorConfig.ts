0bee3a3c37ab5e26a83655b4d41430432181e2f4
LemonLime <111824263+HappyLemonLime@users.noreply.github.com>
2025-09-30 15:13:27 -0700

    Updated the LemmBox default theme and added new plants
    Updated the LemmBox default theme and Added 31 new plants for each day in October.

--- editor/ColorConfig.ts
+++ editor/ColorConfig.ts
@@ -4541,7 +4541,7 @@ export class ColorConfig {
     --link-accent: #fff570;
     --ui-widget-background: #191721;
     --ui-widget-focus: #2d293b;
-    --pitch-background: #44444A;
+    --pitch-background: #443d4a;
     --tonic: #c2a855;
     --fifth-note: #a0cd7c;
     --third-note: #486;
